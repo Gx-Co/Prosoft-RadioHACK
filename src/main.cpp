ee7868e359d8fc12c72f8ca85c118b8c00af70e4
ModestV <vladpolovinka@mail.ru>
2025-10-06 19:38:59 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "auth.hpp"
 
 int main() {
-    std::cout << "Auth server started..." << std::endl;
+    std::cout << "=== Centralized Authorization Server ===" << std::endl;
+    std::cout << "App started successfully!" << std::endl;
+
+    // Заглушка: просто проверяем, что всё работает
+    Auth auth;
+    auth.test();
+
     return 0;
 }
