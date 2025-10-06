d67e8f775b8b720bb76b0144350ec7834fdc9665
ModestV <vladpolovinka@mail.ru>
2025-10-06 19:40:05 +0500

    Create storage.cpp

---
+++ src/storage.cpp
@@ -0,0 +1,12 @@
+#include "storage.hpp"
+#include <iostream>
+
+bool Storage::loadUsers() {
+    std::cout << "[Storage] loadUsers() placeholder" << std::endl;
+    return true;
+}
+
+bool Storage::saveUsers() {
+    std::cout << "[Storage] saveUsers() placeholder" << std::endl;
+    return true;
+}
